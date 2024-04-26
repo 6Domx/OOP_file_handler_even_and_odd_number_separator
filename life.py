b1bf4f4ae6c581719bbd1b9e6e4e4c73aa0fa690
unknown <miggy.payumo26@gmail.com>
2024-04-26 20:12:41 +0800

    Def function building

--- life.py
+++ life.py
@@ -1,3 +1,9 @@
 # PSEUDOCODE
 # 1. Write a method in python to write multiple line of text Contents into a text file mylife.txt.
 
+# MAIN CODE LINE
+
+def multiple_line_writing(filename, lines):
+    with open(filename, "w") as f:
+        f.writelines(lines)
+
