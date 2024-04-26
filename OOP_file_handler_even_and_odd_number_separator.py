43458561920044d530f23bac03a52131a13bb3c4
unknown <miggy.payumo26@gmail.com>
2024-04-26 18:39:55 +0800

    building of the define function

--- OOP_file_handler_even_and_odd_number_separator.py
+++ OOP_file_handler_even_and_odd_number_separator.py
@@ -5,3 +5,14 @@
 # 4. The first text file should be named even.txt and it should contain all the even numbers
 # 5. The second text file should be named odd.txt and it should contain all the odd numbers.
 
+# MAIN CODE LINE
+
+def evn_odd_separate(numbers):
+
+    with open ("numbers.txt", "r") as main_file:
+        numbers = [int(line.strip()) for line in main_file]
+
+    evn_numbers = [num for num in numbers if num % 2 == 0]
+    odd_numbers = [num for num in numbers if num % 2 != 0]
+
+    
