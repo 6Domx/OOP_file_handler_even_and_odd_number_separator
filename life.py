13d0d230cc7569146a87b911d67cdb681b49060b
unknown <miggy.payumo26@gmail.com>
2024-04-26 20:26:51 +0800

    adding input functions

--- life.py
+++ life.py
@@ -3,8 +3,13 @@
 
 # MAIN CODE LINE
 
-with open("mylife.py", "w") as text_file:
+with open("mylife.txt", "w") as text_file:
     while True:
         line_input = input("Enter a new line: ")
         text_file.write (line_input + "\n")
 
+        more_line_input = input("Do you want to input more lines? (y/n): ")
+        if more_line_input() == "y":
+            continue
+        else:
+            break
