025076db11a02f9882c4fce72f4ced13866c86c3
unknown <miggy.payumo26@gmail.com>
2024-04-26 20:47:45 +0800

    resorted back to file name instead of path origin

--- OOP_file_handler_even_and_odd_number_separator.py
+++ OOP_file_handler_even_and_odd_number_separator.py
@@ -9,7 +9,7 @@
 
 def evn_odd_separate(numbers):
 
-    with open ("C:\Users\M2\Documents\CODING\python\OOP_file_handler_even_and_odd_number_separator\numbers.txt", "r") as main_file:
+    with open ("numbers.txt", "r") as main_file:
         numbers = [int(line.strip()) for line in main_file]
 
     evn_numbers = [num for num in numbers if num % 2 == 0]
