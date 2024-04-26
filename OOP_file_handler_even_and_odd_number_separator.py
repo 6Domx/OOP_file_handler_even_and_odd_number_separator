a6b882acc4343521ab02bd6ebdc902f431fd0713
unknown <miggy.payumo26@gmail.com>
2024-04-26 18:23:02 +0800

    Pseudocode

---
+++ OOP_file_handler_even_and_odd_number_separator.py
@@ -0,0 +1,7 @@
+# PSEUDOCODE
+# 1. Create a Program that reads a text file.
+# 2. It should contain 20 integers.
+# 3. The Program should be able to create two more new text file.
+# 4. The first text file should be named even.txt and it should contain all the even numbers
+# 5. The second text file should be named odd.txt and it should contain all the odd numbers.
+
