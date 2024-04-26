00dfeeda6b3905f53c6024f51b3d57d8d1079ee9
unknown <miggy.payumo26@gmail.com>
2024-04-26 19:07:55 +0800

    Pseudocode

---
+++ gwa_highest_to_lowest.py
@@ -0,0 +1,3 @@
+# PSEUDOCODE
+# 1. Create a python program that will read a file containing 20 students' GWA
+# 2. The Program should print the name of the Student who got the highest GWA with their GWA.
