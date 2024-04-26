9889c27c89fa459f975c791e9f74e3029a178569
unknown <miggy.payumo26@gmail.com>
2024-04-26 19:42:50 +0800

    Calling the define function so the code can run

--- gwa_highest_to_lowest.py
+++ gwa_highest_to_lowest.py
@@ -4,16 +4,23 @@
 
 # MAIN CODE LINE
 
-def print_highest_gwa(students_gwa):
-
+def print_highest_gwa(filename):
     highest_student = {"student_name": None, "gwa": 0.0}
-    with open("C:\Users\M2\Documents\CODING\python\OOP_file_handler_even_and_odd_number_separator\students_gwa.txt", "r") as file:
+    with open(filename, "r") as file:
         for line in file:
-            student_name, gwa = line.strip().split()
-            gwa = float(gwa)
+            student_name, gwa_str = line.strip().split()
+            gwa = float(gwa_str)
 
             if gwa > highest_student["gwa"]:
                 highest_student["student_name"] = student_name
                 highest_student["gwa"] = gwa
 
-    return print_highest_gwa
+    return highest_student
+
+if __name__ == "__main__":
+    filename = r"C:\Users\M2\Documents\CODING\python\OOP_file_handler_even_and_odd_number_separator\students_gwa.txt"
+
+    highest_student = print_highest_gwa(filename)
+
+    if highest_student["student_name"]:
+        print(f"Student with the highest GWA: {highest_student['student_name']} ({highest_student['gwa']:.2f})")
