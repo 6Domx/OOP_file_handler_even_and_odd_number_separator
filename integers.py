89b43209fb8d9cd6393dc1bbca1ee7fd708846a4
unknown <miggy.payumo26@gmail.com>
2024-04-26 20:33:36 +0800

    Pseudocode

---
+++ integers.py
@@ -0,0 +1,4 @@
+# PSEUDOCODE 
+# 1. Write a python program that will create two separate text files after reading the source file named integers.
+# 2. First file should contain the square of all integers.
+# 3. Second file should contain the cube of all integers.
